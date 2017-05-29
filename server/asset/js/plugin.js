fa500c790687edc6ca68437f29a14bf26167cff1
ruslana-shev <ruslana.shevandina@gmail.com>
2017-05-29 18:26:51 +0300

    добавлен plagin.js занятие 33

---
+++ server/asset/js/plugin.js
@@ -0,0 +1,48 @@
+'use strict';
+
+;(function ($) {
+	$(window).on('load', function (e) {
+		var taskLink = $('.toggle-task-list a');
+		var taskContent = $('.task-content');
+		var taskHeader = $('.task-header');
+		var addTaskBtn = $('.add-task');
+		var overlay = $('.overlay');
+		var editTaskBlock = $('.edit-task');
+
+		function tabTask(e) {
+			e.preventDefault();
+			var activeBlock = $(this).attr('href');
+			console.log(activeBlock);
+			taskLink.removeClass('active');
+			taskContent.removeClass('active');
+			$(this).addClass('active');
+			$(activeBlock).addClass('active');
+		}
+
+		function taskSlideToggle(e) {
+			if (!$(e.target).hasClass('icon-cancel')) {
+				var parentTask = $(this).closest('.task');
+				var taskContentWrap = $(parentTask).find('.task-content-wrap');
+
+				if ($(parentTask).hasClass('open')) {
+					$(taskContentWrap).slideUp(500, function () {
+						$(parentTask).removeClass('open');
+					});
+				} else {
+					$(taskContentWrap).slideDown(500, function () {
+						$(parentTask).addClass('open');
+					});
+				}
+			}
+		}
+
+		function openEditBlock(e) {
+			$([overlay, editTaskBlock]).toggleClass('open');
+		}
+
+		taskLink.on('click', tabTask);
+		taskHeader.on('click', taskSlideToggle);
+		addTaskBtn.on('click', openEditBlock);
+		overlay.on('click', openEditBlock);
+	});
+})(jQuery);
